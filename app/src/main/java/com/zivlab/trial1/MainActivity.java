9075df2cee9a24a1cc0d4d7224663b43bed8d9dc
yoesak <yusak.setiawan@tigabelas.com>
2017-05-17 03:25:34 +0700

    Initial

---
+++ app/src/main/java/com/zivlab/trial1/MainActivity.java
@@ -0,0 +1,274 @@
+package com.zivlab.trial1;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v7.app.AlertDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.core.CvType;
+import org.opencv.core.KeyPoint;
+import org.opencv.core.Mat;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.MatOfKeyPoint;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.features2d.FeatureDetector;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private Mat img_input_rgba, img_input_gray;
+    private Mat img_result;
+    private static final String TAG = "opencv";
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+    public native int convertNativeLib(long matAddrInput, long matAddrResult);
+
+    public native int test();
+
+    static final int PERMISSION_REQUEST_CODE = 1;
+    String[] PERMISSIONS  = {"android.permission.CAMERA"};
+
+    static {
+        System.loadLibrary("opencv_java3");
+        System.loadLibrary("native-lib");
+    }
+
+    private boolean hasPermissions(String[] permissions) {
+        int ret = 0;
+        for (String perms : permissions){
+            ret = checkCallingOrSelfPermission(perms);
+            if (!(ret == PackageManager.PERMISSION_GRANTED)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void requestNecessaryPermissions(String[] permissions) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(permissions, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    mOpenCvCameraView.enableView();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
+    @Override
+    public void onRequestPermissionsResult(int permsRequestCode, String[] permissions, int[] grantResults){
+        switch(permsRequestCode){
+
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0) {
+                    boolean camreaAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+                        if (!camreaAccepted  )
+                        {
+                            showDialogforPermission("앱을 실행하려면 퍼미션을 허가하셔야합니다.");
+                            return;
+                        }else
+                        {
+                            //이미 사용자에게 퍼미션 허가를 받음.
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    private void showDialogforPermission(String msg) {
+
+        final AlertDialog.Builder myDialog = new AlertDialog.Builder(  MainActivity.this);
+        myDialog.setTitle("알림");
+        myDialog.setMessage(msg);
+        myDialog.setCancelable(false);
+        myDialog.setPositiveButton("예", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(PERMISSIONS, PERMISSION_REQUEST_CODE);
+                }
+
+            }
+        });
+        myDialog.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                finish();
+            }
+        });
+        myDialog.show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().addFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        if (!hasPermissions(PERMISSIONS)) {
+            requestNecessaryPermissions(PERMISSIONS);
+        } else {
+
+        }
+
+        mOpenCvCameraView = (CameraBridgeViewBase)findViewById(R.id.activity_surface_view);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+        mOpenCvCameraView.setCameraIndex(0); // front-camera(1),  back-camera(0)
+        mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "onResume :: Internal OpenCV library not found.");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "onResum :: OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+        img_input_gray = new Mat(height, width, CvType.CV_8UC4);
+        img_input_rgba = new Mat(height, width, CvType.CV_8UC4);
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+
+        img_input_rgba = inputFrame.rgba();
+        img_input_gray = inputFrame.gray();
+        img_result = new Mat();
+
+        convertNativeLib(img_input_rgba.getNativeObjAddr(), img_result.getNativeObjAddr());
+
+        detectText();
+
+        return img_result;
+    }
+
+
+    private void detectText() {
+        Scalar CONTOUR_COLOR = new Scalar(255, 0, 0, 255);
+        MatOfKeyPoint keypoint = new MatOfKeyPoint();
+        List<KeyPoint> listpoint;
+        KeyPoint kpoint;
+        Mat mask = Mat.zeros(img_result.size(), CvType.CV_8UC1);
+        int rectanx1;
+        int rectany1;
+        int rectanx2;
+        int rectany2;
+        int imgsize = img_result.height() * img_result.width();
+        Scalar zeos = new Scalar(0, 0, 0);
+
+        List<MatOfPoint> contour2 = new ArrayList<MatOfPoint>();
+        Mat kernel = new Mat(1, 50, CvType.CV_8UC1, Scalar.all(255));
+        Mat morbyte = new Mat();
+        Mat hierarchy = new Mat();
+
+        Rect rectan3;
+        //
+        FeatureDetector detector = FeatureDetector
+                .create(FeatureDetector.MSER);
+        detector.detect(img_result, keypoint);
+        listpoint = keypoint.toList();
+        //
+        for (int ind = 0; ind < listpoint.size(); ind++) {
+            kpoint = listpoint.get(ind);
+            rectanx1 = (int) (kpoint.pt.x - 0.5 * kpoint.size);
+            rectany1 = (int) (kpoint.pt.y - 0.5 * kpoint.size);
+            rectanx2 = (int) (kpoint.size);
+            rectany2 = (int) (kpoint.size);
+            if (rectanx1 <= 0)
+                rectanx1 = 1;
+            if (rectany1 <= 0)
+                rectany1 = 1;
+            if ((rectanx1 + rectanx2) > img_result.width())
+                rectanx2 = img_result.width() - rectanx1;
+            if ((rectany1 + rectany2) > img_result.height())
+                rectany2 = img_result.height() - rectany1;
+            Rect rectant = new Rect(rectanx1, rectany1, rectanx2, rectany2);
+            try {
+                Mat roi = new Mat(mask, rectant);
+                roi.setTo(CONTOUR_COLOR);
+            } catch (Exception ex) {
+                Log.d("mylog", "mat roi error " + ex.getMessage());
+            }
+        }
+
+        Imgproc.morphologyEx(mask, morbyte, Imgproc.MORPH_DILATE, kernel);
+        Imgproc.findContours(morbyte, contour2, hierarchy,
+                Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
+        for (int ind = 0; ind < contour2.size(); ind++) {
+            rectan3 = Imgproc.boundingRect(contour2.get(ind));
+            rectan3 = Imgproc.boundingRect(contour2.get(ind));
+            if (rectan3.area() > 0.5 * imgsize || rectan3.area() < 100
+                    || rectan3.width / rectan3.height < 2) {
+                Mat roi = new Mat(morbyte, rectan3);
+                roi.setTo(zeos);
+
+            } else
+                Imgproc.rectangle(img_result, rectan3.br(), rectan3.tl(),
+                        CONTOUR_COLOR);
+        }
+    }
+
+}
